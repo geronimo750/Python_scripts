a8f2d875a932f9ce693297baf684563f4c629b91
geronimo750 <marco.geron@gmail.com>
2020-02-17 15:10:00 +0000

    Update HSDT_flatplate.py

--- Hypersonic/HSDT_flatplate.py
+++ Hypersonic/HSDT_flatplate.py
@@ -6,7 +6,7 @@ import mpmath
 gamma=1.4
 Mach=[3,5,10]
 delta=(gamma+1)/2
-alpha=np.linspace(0.1,10,20)
+alpha=np.linspace(0.01,10,20)
 alphar=alpha*np.pi/180
 
 
@@ -16,8 +16,17 @@ for M in Mach:
     Ka = M * np.sin(alphar)
     Cps=(np.sin(alphar))**2*(delta + np.sqrt(delta**2 + 4/Ka**2))
     Cpe=(np.sin(alphar))**2*(2/(gamma*Ka**2)*((1-(gamma-1)/2*Ka)**(2*gamma/(gamma-1))-1))
-    plt.plot(alpha,Cps-Cpe, label= 'M= %.2f'%M, color='black')
-    # plt.plot(alpha,(gamma+1)*alphar**2)
+    plt.plot(alpha,Cps-Cpe, color='black')
     plt.plot(alpha,4*alphar/np.sqrt(M**2-1),'--r')
 
-plt.plot(alpha,2*np.pi*alphar)    
+
+plt.plot(alpha,2*np.pi*alphar)
+plt.ylim(0,0.20)
+plt.xlabel('AoA in deg')
+plt.ylabel ('C_L')
+plt.text(6,.18, 'M=3',{'color':'black', 'fontsize':14})
+plt.text(8,.14, 'M=5',{'color':'black', 'fontsize':14})
+plt.text(8.5,.097, 'M=10',{'color':'black', 'fontsize':14})
+plt.legend(('_nolegend_' ,'_nolegend_' ,'_nolegend_' ,'_nolegend_' ,'Hyp. Sim. par.','small perturbation','incompressible'))
+
+plt.show()
