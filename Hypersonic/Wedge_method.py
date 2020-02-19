7a42afbf4753e9361dcda1fc0ef20824b0702068
geronimo750 <marco.geron@gmail.com>
2020-02-19 14:42:10 +0000

    Update Wedge_method.py

--- Hypersonic/Wedge_method.py
+++ Hypersonic/Wedge_method.py
@@ -4,10 +4,18 @@ from sympy import *
 
 
 beta=symbols('beta')
-theta=np.deg2rad(37)
+theta=np.deg2rad(10)
 M=8
 
 # expr=((M**2)*(sin(beta))**2-1-(1.4+1)/2*(M**2)*(sin(beta)*sin(theta))/cos(beta-theta))
 expr = Eq((M**2)*(sin(beta))**2 - 1 , (1.4 + 1) / 2 * (M**2) * (sin(beta) * sin(theta)) / cos(beta - theta))
 
 sol=nsolve(expr,beta,(0.7))
+
+Mn=M*sin(sol)
+
+gam=1.4
+
+p2p1=1+2*gam/(gam+1)*(Mn**2-1)
+
+cp=2/(gam*M**2)*(p2p1-1)
