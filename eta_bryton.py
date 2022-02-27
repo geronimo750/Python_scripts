f15b9cdd785656276dad860f3dc5a59da6ccd126
geronimo750 <marco.geron@gmail.com>
2022-02-27 22:38:06 +0000

    Update eta_bryton.py

--- eta_bryton.py
+++ eta_bryton.py
@@ -27,13 +27,26 @@ gam=1.3
 eta3=1-(1/PR)**((gam-1)/gam)
 
 plt.rc('font', size=20)
-p1=sp.plot(eta1,(PR,1,80),ylabel='Thermal efficiency $\eta_{th}$', show=False, label='$\gamma$=1.67', legend=True)
-p2=sp.plot(eta2,(PR,1,80),ylabel='Thermal efficiency $\eta_{th}$', show=False, label='$\gamma$=1.4',legend=True)
-p3=sp.plot(eta3,(PR,1,80),ylabel='Thermal efficiency $\eta_{th}$', show=False, label='$\gamma$=1.33',legend=True)
+p1=sp.plot(eta1,(PR,1,80),ylabel='Thermal efficiency $\eta_{th}$', show=False )
+p2=sp.plot(eta2,(PR,1,80),ylabel='Thermal efficiency $\eta_{th}$', show=False )
+p3=sp.plot(eta3,(PR,1,80),ylabel='Thermal efficiency $\eta_{th}$', show=False )
 
-p1.extend(p2)
-p1.extend(p3)
+x1, y1 =p1[0].get_points()
+x2, y2 =p2[0].get_points()
+x3, y3 =p3[0].get_points()
 
-p1.show()
+
+fig, ax = plt.subplots()
+
+plt.plot(x1,y1,label='$\gamma$=1.67' )
+plt.plot(x2,y2,label='$\gamma$=1.4')
+plt.plot(x3,y3,label='$\gamma$=1.33')
+
+ax.set_xlabel('$r_p$')
+ax.set_ylabel('Thermal efficiency $\eta_{th}$')
+
+plt.legend()
+
+plt.show()
 
 
